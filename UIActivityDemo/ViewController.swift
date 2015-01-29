eeba8ddf0aa6b58fd43bce0a3f7fccf2d79ebd2c
meenakshi-mindfire <meenakship@mindfiresolutions.com>
2015-01-29 13:25:33 +0530

    Initial Commit

--- UIActivityDemo/ViewController.swift
+++ UIActivityDemo/ViewController.swift
@@ -20,6 +20,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func shareAction(sender: AnyObject) {
+        
+        let text = "Text to share"
+        let url : AnyObject! = NSURL(string: "https://www.google.co.in")
+        let image : AnyObject = UIImage(named:"download.jpeg")!
+        let activityItems : [AnyObject] = [text, url, image]
+        
+        let activityViewController : UIActivityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        activityViewController.excludedActivityTypes = [UIActivityTypeAirDrop];
+        self.presentViewController(activityViewController, animated: true, completion: nil)
+        
+        activityViewController.completionWithItemsHandler = {
+            (activity, success, items, error) in
+            println("Activity: \(activity) Success: \(success) Items: \(items) Error: \(error)")
+        }
+        
+    }
+
 
 }
 
